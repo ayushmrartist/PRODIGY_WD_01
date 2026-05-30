3a40553943c954501a7fa3dd1824fc2a15dced7f
AYUSH KUMAR RAM <ayushkumarjj@gmail.com>
2026-05-31 00:37:32 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,10 @@
+//using the addeventlistener to triggere an event. and then the condition.
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
